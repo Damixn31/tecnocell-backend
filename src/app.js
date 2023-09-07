de81025e584fcfeef28bcb20dd50b1fd7f7a0ff1
Damixn31 <damianol29@hotmail.com>
2023-09-06 22:32:43 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -8,6 +8,14 @@ import indexRoutes from './routes/index.routes.js';
 
 const app = express(); 
 
+//cors
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next()
+})
+
 app.use(express.json());
 
 app.use(indexRoutes)
